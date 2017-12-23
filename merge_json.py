314c109f846e2694d058cd99cd37b2f399ebb1cf
elicassion <elicassion@sjtu.edu.cn>
2017-12-23 13:47:45 +0800

    emmmmmm encode problem

--- merge_json.py
+++ merge_json.py
@@ -1,10 +1,11 @@
 import json
+import codecs
 
 f = open('reviews_Video_Games_5_strict.json', 'r')
 
 content = f.read()
-content = ',\n'.join(content.split('\n'))
+content = ',\n'.join(content.split('\n')[:-1])
 content = '[' + content + ']'
 
-q = open('reviews_Video_Games_5_array.json', 'w')
+q = codecs.open('reviews_Video_Games_5_array.json', 'w', 'ascii')
 q.write(content)
